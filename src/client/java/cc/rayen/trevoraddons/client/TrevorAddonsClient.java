a27558eec944a0597c974e4657716ae18f3b8363
rayen <rayenrathmann@gmail.com>
2026-05-16 20:08:33 +0200

    Initial commit

---
+++ src/client/java/cc/rayen/trevoraddons/client/TrevorAddonsClient.java
@@ -0,0 +1,10 @@
+package cc.rayen.trevoraddons.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrevorAddonsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/cc/rayen/trevoraddons/TrevorAddons.java
@@ -0,0 +1,10 @@
+package cc.rayen.trevoraddons;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TrevorAddons implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
